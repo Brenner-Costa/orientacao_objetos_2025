12b5375f6447deb6fc010d28b031f9578a89033d
Brenner Costa <costabrenner1@gmail.com>
2025-04-11 09:29:09 -0300

    criando o sistema para vendas de ingressos // ajustando metodos do cliente, evento e ingresso

--- src/app/Ingresso.java
+++ src/app/Ingresso.java
@@ -1,3 +1,15 @@
-public class Ingresso {
-    
+public abstract class Ingresso implements IPagamento {
+    protected int id;
+    protected Cliente cliente;
+    protected Evento evento;
+
+    public Ingresso(int id, Cliente cliente, Evento evento) {
+        this.id = id;
+        this.cliente = cliente;
+        this.evento = evento;
+    }
+
+    public Evento getEvento() { return evento; }
+    public Cliente getCliente() { return cliente; }
+    public int getCodigo() { return id; } // Levando em consideração que o ID é único para cada ingresso, usei o próprio ID como código do ingresso.
 }
